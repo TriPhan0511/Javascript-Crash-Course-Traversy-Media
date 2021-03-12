f32278a6e3e1be2eeef83bea1cc5f4e95804bf35
Tri Phan <phanphutri@gmail.com>
2021-03-12 21:01:07 +0700

    Update main.js

--- main.js
+++ main.js
@@ -24,8 +24,53 @@
 // 2. Data types
 
 // Primtitive type: String, Numbers, Boolean, null, undefined, Symbol
+// const name = 'John';
+// const age = 30;
+// const rating = 4.5;
+// const isCool = true;
+// const x = null;
+// const y = undefined;
+// let z;
 
-const name = 'John';
-const age = 30;
-const rating = 4.5;
-const isCool = true;
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof rating);
+// console.log(typeof isCool);
+// console.log(typeof x);
+// console.log(typeof y);
+// console.log(typeof z);
+//----------------------------------------------------------------------------------------
+
+// string
+// const userName = 'John';
+// const age = 30;
+
+// Concatenation
+// Template Literal
+// console.log(`My name is ${userName} and I am ${age}`);
+// const hello = `My name is ${userName} and I am ${age}`;
+// console.log(hello);
+
+// Properties and Methods
+
+// Array's method
+// const arr = ['Hello', 'there', 'how', 'are', 'you'];
+// const str = arr.join(' ');
+// console.log(str);
+
+// String's method
+// const str = 'Hello world';
+// const arr = str.split(' ');
+// console.log(arr);
+
+const str = 'Hello world';
+// console.log(str.length); // 11
+// console.log(str.toUpperCase());
+// console.log(str.startsWith('H'));
+// console.log(str.endsWith('e'));
+// console.log(str.indexOf('K')); // -1
+// console.log(str.lastIndexOf('o')); // 7
+// console.log(str.replace('H', 'K'));
+// console.log(str);
+// const newStr = str.replace('ll', 'a');
+// console.log(newStr);
