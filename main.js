4ac9703095e7dd9b8fe3d1ad980d6ea71ca35c1c
Tri Phan <phanphutri@gmail.com>
2021-03-12 19:44:00 +0700

    Create index.html, main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+// 1. Variables
+
+// Keywords: let, var
+
+// let age = 30;
+// console.log(age);
+// age = 40;
+// console.log(age);
+
+// const age = 20;
+// console.log(age);
+// age = 100; // Error
+// console.log(agex);
+
+// let score;
+// score = 100;
+// console.log(score);
+
+// const score;
+// score = 100;
+// console.log(score);
+//----------------------------------------------------------------------------------------
+
+// 2. Data types
+
+// Primtitive type: String, Numbers, Boolean, null, undefined, Symbol
+
+const name = 'John';
+const age = 30;
+const rating = 4.5;
+const isCool = true;
